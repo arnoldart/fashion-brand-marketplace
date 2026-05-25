14b82842717b21c78206257706735bc6fd14d48c
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2026-05-25 22:47:14 +0700

    feat: initialize AppModule with Logger and Health modules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
 import { LoggerModule } from './common/logger/logger.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [LoggerModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
 
